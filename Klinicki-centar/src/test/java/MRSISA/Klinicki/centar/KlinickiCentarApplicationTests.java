4c4f8a97f64005c19c67c75a8df98f9b4feb4d11
GDBxNS <51819374+GDBxNS@users.noreply.github.com>
2020-04-05 14:58:07 +0200

    Inicijalizacija projekta
    Napravljen inicijalni projekat

---
+++ Klinicki-centar/src/test/java/MRSISA/Klinicki/centar/KlinickiCentarApplicationTests.java
@@ -0,0 +1,16 @@
+package MRSISA.Klinicki.centar;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KlinickiCentarApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
