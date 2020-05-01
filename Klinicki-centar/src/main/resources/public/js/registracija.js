89077cd4e4b66cdcefdb36fa2859d7681200e920
Igor <igi.l.1999@gmail.com>
2020-05-01 19:02:00 +0200

    Dopunjena forma za registraciju:

--- Klinicki-centar/src/main/resources/public/js/registracija.js
+++ Klinicki-centar/src/main/resources/public/js/registracija.js
@@ -4,8 +4,11 @@ $(document).ready(function(){
 	$("#jmbgError").css('visibility', 'hidden')
 	$("#emailError").css('visibility', 'hidden')
 	$("#lozinkaError").css('visibility', 'hidden')
+	$("#lozinkaConfError").css('visibility', 'hidden')
 	
 	var regex = /^[a-zA-Z]{1,20}$/;
+	var regPass = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+	var regEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	
 	if($("#ime").val().length >20){
 		$("#imeError").text("Ime moze da sadrzi maksimalno 20 karaktera!").css('visibility', 'visible').css('color', 'red');
@@ -22,13 +25,18 @@ $(document).ready(function(){
 		return;
 	}
 	
-	if($("#email").val().length >20){
+	if($("#email").val().length >128){
 		$("#emailError").text("Email moze da sadrzi maksimalno 20 karaktera!").css('visibility', 'visible').css('color', 'red');
 		return;
 	}
 	
-	if($("#lozinka").val().length >20){
-		$("#lozinkaError").text("Lozinka moze da sadrzi maksimalno 20 karaktera!").css('visibility', 'visible').css('color', 'red');
+	if($("#lozinka").val().length >256){
+		$("#lozinkaError").text("Lozinka moze da sadrzi maksimalno 256 karaktera!").css('visibility', 'visible').css('color', 'red');
+		return;
+	}
+	
+	if($("#lozinka_conf").val().length >256){
+		$("#lozinkaConfError").text("Lozinka moze da sadrzi maksimalno 256 karaktera!").css('visibility', 'visible').css('color', 'red');
 		return;
 	}
 	
@@ -57,6 +65,11 @@ $(document).ready(function(){
         return;
     }
 	
+	if ($("#lozinka_conf").val() == "") {
+        $("#lozinkaConfError").text("Morate potvrditi lozinku!").css('visibility', 'visible').css('color', 'red');
+        return;
+    }
+	
 	if (!regex.test($("#ime").val())) {
         $("#imeError").text("Ime moze da sadrzi samo mala ili velika slova").css('visibility', 'visible').css('color', 'red');
         return;
@@ -67,29 +80,45 @@ $(document).ready(function(){
         return;
     }
 	
+	if(!regEmail.test($("#email").val())){
+		$("#emailError").text("Neispravan format email-a").css('visibility', 'visible').css('color', 'red');
+		return;
+	}
 	
-	$("#potvrdi").click(function(){
-		$.ajax({
-			type : 'POST',
-			url : "/klinicki-centar/pacijent/register",
-			dataType : "json",
-			contentType:"application/json",
-			data : JSON.stringify({
-				id: imeParam,
-				ime : $("#ime").val(),
-				prezime : $("#prezime").val(),
-				jmbg: $("#jmbg").val(),
-				email : $("#email").val(),
-				lozinka : $("#lozinka").val()
-			}),
-
-			success : function(response) {
-				alert("Uspesno promenjeno")
-				printPacijent(response);
-			},
-			error : function(response) {
-				alert("Greska pri kliku potvrdi")
-			}
-		});
+	if (!regPass.test($("#lozinka").val())){
+		$("#lozinkaError").text("Lozinka mora da sadrži najmanje 8 karaktera, bar jedno malo slovo, bar jedno veliko slovo i bar jedan broj").css('visibility', 'visible').css('color', 'red');
+		return;
+	}
+	
+	if (!regPass.test($("#lozinka_conf").val())){
+		$("#lozinkaConfError").text("Lozinka mora da sadrži najmanje 8 karaktera, bar jedno malo slovo, bar jedno veliko slovo i bar jedan broj").css('visibility', 'visible').css('color', 'red');
+		return;
+	}
+	
+	if($("#lozinka").val() != $("#lozinka_conf").val()){
+		$("#lozinkaConfError").text("Mora da se poklapa sa lozinkom").css('visibility', 'visible').css('color', 'red');
+		return;
+	}
+	
+	
+	$.ajax({
+		type: "POST",
+		contentType: "application/json",
+		url: "/klinicki-centar/pacijent/add",
+		dataType: 'json',
+		data: JSON.stringify({
+			ime: $("#ime").val(),
+			prezime: $("#prezime").val(),
+			jmbg: $("#jmbg").val(),
+			email: $("#email").val(),
+			lozinka: $("#lozinka").val(),
+			pol: $("#pol").val()
+		}),
+		success: function(){
+			window.location.replace("pacijenti.html")
+		},
+		error: function(){
+			alert("Error in call /pacijenti/add")
+		}
 	});
 }
