fe5f42563a7b91a7e860d208bf62d22ec27e56c4
Igor <igi.l.1999@gmail.com>
2020-05-08 19:37:48 +0200

    Skripta3

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
-cd $TRAVIS_BUILD_DIR/project
-sbt ++$TRAVIS_SCALA_VERSION package
+cd Klinicki-centar
+mvnw clean install -D skipTests=false -B
+mvnw sonar:sonar -D sonar.projectKey=cigor99_MRSISA2020_T19 -D sonar.organization=tim19 -D sonar.host.url=https://sonarcloud.io -D sonar.login=bd5e21c61e921f61bab61f7e27518c4810286ee8
